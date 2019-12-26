d255a2ca1a39686a8de85e8a06c4142df7d28e69
Lorenzo Vidaurre More <desarrollo.software06@servex.com.pe>
2019-12-26 14:57:31 -0500

    version 0.0.1 s Please enter the commit message for your changes. Lines starting

---
+++ webServex/src/test/java/chat/WebAndroid.java
@@ -0,0 +1,39 @@
+package chat;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class WebAndroid {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		File f = new File("src/test/java/");
+		File fs= new File(f,"ApiDemos-debug.apk");	
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+	//	cap.setCapability("platformName", "Android");
+		cap.setCapability("deviceName", "AOSP on IA Emulator");
+		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+	//	cap.setCapability("deviceName", "Android");
+	//	cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+	//	cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+//		cap.setCapability("appPackage", "com.linkedin.android");
+//		cap.setCapability("appActivity", "com.linkedin.android.authenticator.LaunchActivity");
+//		cap.setCapability("noreset", true);
+	//	cap.setCapability("autoWebview", true);
+	//	cap.setCapability("appActivity", "com.linkedin.android.webrouter.webviewer.WebViewerActivity");
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+		//driver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+	//	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	//	return driver;
+	}
+
+}
